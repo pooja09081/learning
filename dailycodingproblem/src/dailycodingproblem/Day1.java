7575fe7df444786d347594bc9d2e3e19927934ed
pooja09081 <poojagsagar9896@gmail.com>
2022-08-28 00:00:25 +0530

    Day1 setup commits

---
+++ dailycodingproblem/src/dailycodingproblem/Day1.java
@@ -0,0 +1,26 @@
+package dailycodingproblem;
+
+public class Day1 implements ExecutableProblem {
+
+	@Override
+	public void execute() {
+		
+		int[] test1 = {1,2,3,4,5};
+		int[] test2 = {1,2,3,4,5};
+		int[] test3 = {1,2,3,4,5};
+		
+		
+		System.out.println("Test case 1: "+ checkIfTwoSumExists(test1,10));
+	}
+	
+	/*
+	 * 
+	 * 
+	 * 
+	 * */
+	
+	private boolean checkIfTwoSumExists(int[] a, int target) {
+		
+		return false;
+	}
+}
--- dailycodingproblem/src/dailycodingproblem/Main.java
+++ dailycodingproblem/src/dailycodingproblem/Main.java
@@ -4,7 +4,9 @@ public class Main {
 	
 	public static void main(String[] args) {
 		
-		System.out.println("My new Git Project");
+		ExecutableProblem ex = new Day1();
+		
+		ex.execute();
 	}
 
 }
